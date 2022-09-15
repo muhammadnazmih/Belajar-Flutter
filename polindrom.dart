656493e51a2d5bc1037bdb7c30d503839cc5452d
muhammadnazmih <52620592+muhammadnazmih@users.noreply.github.com>
2022-09-15 16:31:31 +0700

    Membuat program polindrom

---
+++ polindrom.dart
@@ -0,0 +1,32 @@
+void main() {
+  List<String> Value = ["Kata", "KAKaK", "Adik", "Nababan"];
+
+  ///Mencari Palindrom dengan membalikkan kata
+
+/*   String temp = "";
+  String kata = "kakak";
+
+  for (var i = kata.length - 1; i >= 0; i--) {
+    kata = kata.toLowerCase();
+    temp = temp + String.fromCharCode(kata.codeUnitAt(i));
+  }
+  (temp == kata) ? print("palindrom") : print("Bukan palindrom"); */
+
+  //Mencari Palindrom dengan membandingkan kata pertama dan terakhir
+
+  for (var kata in Value) {
+    kata = kata.toLowerCase();
+
+    print(
+        "Kata $kata ${isPalindrom(kata) ? "adalah" : "Bukan"}" + " Palindrom");
+  }
+}
+
+bool isPalindrom(String kata) {
+  for (var i = 0; i < kata.length / 2; i++) {
+    if (kata[i] != kata[kata.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
